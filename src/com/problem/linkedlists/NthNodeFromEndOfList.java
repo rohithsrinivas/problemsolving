42e2365eed44ec8f7316b0377caf2f7e0a4cb06b
Srinivas rohith Ravulavaru <M1037764@mindtree.com>
2018-09-04 10:02:12 +0530

    adding alterntive method using two pointers

--- src/com/problem/linkedlists/NthNodeFromEndOfList.java
+++ src/com/problem/linkedlists/NthNodeFromEndOfList.java
@@ -1,49 +1,65 @@
 package com.problem.linkedlists;
 
 public class NthNodeFromEndOfList {
-	
+
 	Node head;
-	
+
 	class Node {
 		int data;
 		Node next;
-		
+
 		public Node(int data) {
 			this.data = data;
 			this.next = null;
 		}
 	}
-	
+
 	private void insertInFirstPlace(int data) {
 		Node newNode = new Node(data);
 		newNode.next = head;
 		head = newNode;
 	}
-	
+
 	private void insertInLastPlace(int data) {
+		if(head == null) {
+			insertInFirstPlace(data);
+			return;
+		}
 		Node temp = head;
-		while(temp.next!=null) {
+		while (temp.next != null) {
 			temp = temp.next;
 		}
-		
+
 		Node newNode = new Node(data);
 		temp.next = newNode;
 		newNode.next = null;
 	}
-	
+
 	private void printNthNodeFromEnd(int position) {
 		Node temp = head;
-		for(int i=0;i<getSizeOfList()-(position);i++) {
+		for (int i = 0; i < getSizeOfList() - (position); i++) {
 			temp = temp.next;
 		}
-		
-		System.out.println("the required node data is "+temp.data);
+
+		System.out.println("the required node data is " + temp.data);
 	}
-	
+
+	private void printNthNodeFromEndUsing2Pointers(int position) {
+		Node slow = head, fast = head;
+		for (int i = 1; i < position; i++) {
+			fast = fast.next;
+		}
+		while(fast.next != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+		System.out.println(" the "+position+"th node is "+slow.data);
+	}
+
 	private int getSizeOfList() {
 		Node temp = head;
 		int count = 0;
-		while(temp!=null) {
+		while (temp != null) {
 			count++;
 			temp = temp.next;
 		}
@@ -52,25 +68,22 @@ public class NthNodeFromEndOfList {
 
 	private void printList() {
 		Node temp = head;
-		while(temp!=null) {
+		while (temp != null) {
 			System.out.println(temp.data);
-			temp=temp.next;
+			temp = temp.next;
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
 		NthNodeFromEndOfList list = new NthNodeFromEndOfList();
-		list.insertInFirstPlace(10);
-		for(int i=20;i<=50;i=i+10) {
+		for (int i = 10; i <= 50; i = i + 10) {
 			list.insertInLastPlace(i);
 		}
-		
 		list.printList();
-		list.printNthNodeFromEnd(5);
+		list.printNthNodeFromEndUsing2Pointers(1);
+		list.printNthNodeFromEnd(4);
 
 	}
-
-	
 
 }
